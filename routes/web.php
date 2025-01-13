d48bdce0322b959dee0df411c520011c4a1c4080
M-Khurram <khuramafzal@gmail.com>
2025-01-13 22:03:27 +0500

    Update index Router and Product router

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', [ProductController::class, 'index']);
-
+ 
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products', [ProductController::class, 'store']);
 Route::put('/products/{id}', [ProductController::class, 'update']);
